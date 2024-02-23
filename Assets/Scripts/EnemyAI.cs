1b6fc1b7e55ce6bdab107ee4e4bf4780e325ea60
Morgan Houston <mmhouston6@live.com>
2024-02-23 06:02:45 -0600

    Enemy, AI, Baked

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    public Transform target;
+    private NavMeshAgent ai;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ai = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ai.SetDestination(target.position);
+    }
+}
